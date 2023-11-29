10c78d3ac0caf444e7fcc2700f5d4471a47ba340
Anselmo Mancini <anselmo.mancini@hsl.org.br>
2023-11-29 09:59:48 -0300

    Preenchendo a interface com os nomes das estruturas que possuem objetivos de otimização

--- UserInterface.xaml.cs
+++ UserInterface.xaml.cs
@@ -28,6 +28,7 @@ namespace RescaleOptimizationObjectives
             InitializeComponent();
             _planSetup = planSetup;
             LoadCurrentDoseFractionation();
+            LoadStructuresWithOptimizationObjective();
         }
 
         private void LoadCurrentDoseFractionation()
@@ -35,6 +36,35 @@ namespace RescaleOptimizationObjectives
             currentDosePerFractionTextBlock.Text = _planSetup.DosePerFraction.ValueAsString;
             currentNumberOfFractionsTextBlock.Text = _planSetup.NumberOfFractions.ToString();
             currentTotalDoseTextBlock.Text = _planSetup.TotalDose.ValueAsString;
+        }
+
+        private void LoadStructuresWithOptimizationObjective()
+        {
+            foreach (var structureId in _planSetup.OptimizationSetup.Objectives.Select(x => x.StructureId).Distinct())
+            {
+                TextBlock structureIdTextBlock = new TextBlock
+                {
+                    Text = structureId
+                };
+
+                TextBox factorTextBox = new TextBox
+                {
+                    Width = 40,
+                    Margin = new Thickness(5, 0, 0, 0)
+                };
+
+                StackPanel stackPanel = new StackPanel
+                {
+                    Orientation = Orientation.Horizontal,
+                    HorizontalAlignment = HorizontalAlignment.Right,
+                    Margin = new Thickness(0, 0, 0, 5)
+                };
+
+                stackPanel.Children.Add(structureIdTextBlock);
+                stackPanel.Children.Add(factorTextBox);
+
+                mainStackPanel.Children.Add(stackPanel);
+            }
         }
     }
 }
